fcfa50f39ee0a0f885172b270f53d4590464bbe9
Wes Lawrence <wes@axistech.co>
2026-03-19 08:54:23 +0800

    fix: force-dynamic conflict

--- app/keystatic/[[...params]]/page.tsx
+++ app/keystatic/[[...params]]/page.tsx
@@ -5,6 +5,4 @@ export const dynamic = 'force-dynamic'
 import { makePage } from '@keystatic/next/ui/app'
 import config from '../../../keystatic.config'
 
-export const dynamic = 'force-dynamic'
-
 export default makePage(config)
